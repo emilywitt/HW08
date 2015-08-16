76f69f6bbee74a965ac979b1eace9f33616f950f
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-16 09:00:28 -0700

    EW HW08_ex11_05

--- HW08_ex11_05.py
+++ HW08_ex11_05.py
@@ -11,6 +11,9 @@
 #    '1: ["this", "that"]'
 #    '2:'
 #    '3: ["the"]'
+
+
+#max dictionary is a thing
 ##############################################################################
 
 def invert_dict_old(d):
@@ -25,17 +28,49 @@ def invert_dict_old(d):
 
 
 def invert_dict_new(d):
-    pass
-
+    inverse = dict()
+    for key, val in d.items():
+        inverse.setdefault(val, []).append(key)
+    return inverse
 
 def print_hist_newest(d):
-    pass
+    min = 0
+    max1 = max(d)
+    for key in range(max1):
+        min += 1
+        if d.get(min, 0) == 0:
+            d[min] = []
+    return d
+    
 
 ##############################################################################
 ################### INSERT COMPLETED CODE FROM 11_02 BELOW: ##################
 ##############################################################################
 
+def histogram_old(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
 
+def histogram_new(s):
+    d = {}
+    for c in s:
+        d[c] = d.get(c,0) + 1
+    return d
+
+def get_pledge_list():
+    """ Opens pledge.txt and converts to a list, each item is a word in 
+    the order it appears in the original file. returns the list.
+    """
+    with open('pledge.txt', 'r') as fin:
+        lines = fin.read()
+        s = ''.join(c for c in lines if c not in ('.',';',':',','))  
+        pledge_list = s.split()
+        return pledge_list
 
 
 ##############################################################################
