7720f5994b2b4fe51a8cc920e3de373dfa64d03b
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-16 08:59:50 -0700

    EW HW08_ex11_03

--- HW08_ex11_03.py
+++ HW08_ex11_03.py
@@ -18,15 +18,39 @@ def print_hist_old(h):
 
 
 def print_hist_new(h):
-    pass
+    keys_list = dict.keys(h)
+    print sorted(keys_list, key=lambda keys_list: keys_list.lower())
 
 
 ##############################################################################
 ################### INSERT COMPLETED CODE FROM 11_02 BELOW: ##################
 ##############################################################################
 
+def histogram_old(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    print d
 
+def histogram_new(s):
+    d = {}
+    for c in s:
+        d[c] = d.get(c,0) + 1
+    return d
+    
 
+def get_pledge_list():
+    """ Opens pledge.txt and converts to a list, each item is a word in 
+    the order it appears in the original file. returns the list.
+    """
+    with open('pledge.txt', 'r') as fin:
+        lines = fin.read()
+        s = ''.join(c for c in lines if c not in ('.',';',':',','))  
+        pledge_list = s.split()
+        return pledge_list
 
 ##############################################################################
 ################### INSERT COMPLETED CODE FROM 11_02 ABOVE: ##################
@@ -36,7 +60,7 @@ def main():
     """ Calls print_hist_new with the appropriate arguments to print the 
     histogram of pledge.txt.
     """
-    pass
+    print print_hist_new(histogram_new(get_pledge_list()))
 
 if __name__ == '__main__':
     main()
